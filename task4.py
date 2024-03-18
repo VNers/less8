f63910b5a274280eb11f4d4e65a3ac3c5aeb0864
Lera Ners <nersesova.ushko@gmail.com>
2024-03-18 09:46:05 +0200

    Added file

---
+++ task4.py
@@ -0,0 +1,11 @@
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+c = int(input("Enter number c: "))
+
+
+def numbers_avarage(a, b, c):
+    return (a + b + c) / 3
+
+
+avarage = numbers_avarage(a, b, c)
+print("Result = ", avarage)
